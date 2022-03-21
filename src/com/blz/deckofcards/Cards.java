60ff48eb46de0d86c3d0c769c03f35bdcafe1328
rohgr <rdagrawal6@gmail.com>
2022-03-21 08:56:41 +0530

    Adding Deck of Cards program

---
+++ src/com/blz/deckofcards/Cards.java
@@ -0,0 +1,26 @@
+package com.blz.deckofcards;
+
+public class Cards {
+    public static void main(String[] args) {
+        System.out.println("welcome to Deck of cards game");
+
+        int[] deck = new int[52];
+        String[] suits ={"Spades","Hearts","Diamonds","Clubs"};
+        String[] ranks ={"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","king"};
+
+        for (int i = 0; i< deck.length;i++){
+            deck[i]=i;
+        }
+        for(int i=0;i<deck.length;i++){
+            int index =(int)(Math.random() * deck.length);
+            int temp=deck[i];
+            deck[i]=deck[index];
+            deck[index]=temp;
+        }
+        for (int i =0; i<52; i++){
+            String suit=suits[deck[i]/13];
+            String rank=ranks[deck[i] % 13];
+            System.out.println("card number " + deck[i] +": "+ rank + " of " +suit);
+        }
+    }
+}
